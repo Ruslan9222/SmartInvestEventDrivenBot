53ee379423790ed276a97b1ac6e74dceba6ed7b2
Ruslan9222 <magog21@gmail.com>
2026-06-23 00:15:36 +0300

    git add

---
+++ src/main/java/by/ruslan/radzevich/smartinvesteventdrivenbot/SmartInvestEventDrivenBotApplication.java
@@ -0,0 +1,13 @@
+package by.ruslan.radzevich.smartinvesteventdrivenbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInvestEventDrivenBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInvestEventDrivenBotApplication.class, args);
+	}
+
+}
