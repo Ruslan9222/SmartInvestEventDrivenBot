018a834dd796444e84d678e6e3edb7115dbf7957
Ruslan9222 <magog21@gmail.com>
2026-06-23 23:02:25 +0300

    add base config tg bot

---
+++ TgBotService/src/main/java/by/ruslan/radzevich/tgbotservice/bot/BotConfig.java
@@ -0,0 +1,18 @@
+package by.ruslan.radzevich.tgbotservice.bot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(SmartInvestBot smartInvestBot) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(smartInvestBot);
+        return botsApi;
+    }
+}
---
+++ TgBotService/src/main/java/by/ruslan/radzevich/tgbotservice/bot/SmartInvestBot.java
@@ -0,0 +1,49 @@
+package by.ruslan.radzevich.tgbotservice.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Component
+public class SmartInvestBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.name}")
+    private String botUsername;
+
+    public SmartInvestBot(@Value("${telegram.bot.token}") String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            System.out.println("Получен текст: " + messageText + " от чата: " + chatId);
+
+            sendTextMessage(chatId, "Вы написали: " + messageText);
+        }
+    }
+
+    public void sendTextMessage(long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Ошибка при отправке сообщения в Telegram для чата {}: ", chatId, e);
+        }
+    }
+}
