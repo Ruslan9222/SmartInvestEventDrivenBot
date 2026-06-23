883534321fc91b9c306ce6598bc7db3c339bbfd3
Ruslan9222 <magog21@gmail.com>
2026-06-23 22:26:26 +0300

    git add

---
+++ CoreService/src/test/java/by/ruslan/radzevich/coreservice/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ruslan.radzevich.coreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ TgBotService/src/main/java/by/ruslan/radzevich/tgbotservice/TgBotServiceApplication.java
@@ -0,0 +1,13 @@
+package by.ruslan.radzevich.tgbotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgBotServiceApplication.class, args);
+    }
+
+}
